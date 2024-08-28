eccaa89feb13452ed61782ca95ac2e6803a8c122
Felipe Queiroz <felipedsqueiroz94@gmail.com>
2024-08-27 21:57:41 -0300

    Projeto inicial gerado pelo Spring Initializr

---
+++ src/main/java/dev/felipequeiroz/simplepms/SimplepmsApplication.java
@@ -0,0 +1,13 @@
+package dev.felipequeiroz.simplepms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplepmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplepmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/felipequeiroz/simplepms/SimplepmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.felipequeiroz.simplepms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplepmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
