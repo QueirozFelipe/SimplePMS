a099ab27de661cf3da1a438451f8fa649af45120
Felipe Queiroz <felipedsqueiroz94@gmail.com>
2024-08-29 18:02:52 -0300

    fix: corrige o retorno da aplicação quando dados enviados não são validados

--- src/main/java/dev/felipequeiroz/simplepms/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/felipequeiroz/simplepms/infra/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package dev.felipequeiroz.simplepms.infra.exception;
 
 import dev.felipequeiroz.simplepms.exception.BusinessValidationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessValidationException.class)
     public ResponseEntity tratarBusinessValidationException(BusinessValidationException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 
 }
