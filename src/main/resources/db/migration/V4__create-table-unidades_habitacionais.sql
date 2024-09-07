ff396749cb32b2f3e93c1d63979fa303e009ee69
Felipe Queiroz <felipedsqueiroz94@gmail.com>
2024-09-07 17:12:18 -0300

    feature: criada entidade jpa e migration para o cadastro de uh no banco

---
+++ src/main/resources/db/migration/V4__create-table-unidades_habitacionais.sql
@@ -0,0 +1,11 @@
+create table unidades_habitacionais(
+
+    id bigserial not null,
+    nomeUh varchar(255) not null,
+    categoria_de_uh_id bigint not null,
+    ativo boolean,
+
+    primary key(id),
+    constraint fk_unidades_habitacionais_categoria_id foreign key(categoria_de_uh_id) references categorias_de_uh(id)
+
+);
